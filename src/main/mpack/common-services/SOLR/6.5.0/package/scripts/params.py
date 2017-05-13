7d502170185b09c394d533b0033ca2998acbe3db
wsolaligue <wsolaligue@gmail.com>
2017-05-12 21:33:18 -0500

    APOLLO-8956: Move solr_hdfs_prefix out of the if block

--- src/main/mpack/common-services/SOLR/6.5.0/package/scripts/params.py
+++ src/main/mpack/common-services/SOLR/6.5.0/package/scripts/params.py
@@ -84,9 +84,9 @@ kinit_path_local = get_kinit_path(default('/configurations/kerberos-env/executab
 # solr + HDFS
 map_solr_hdfs = config['configurations']['solr-hdfs']
 solr_hdfs_enable = bool(map_solr_hdfs['solr_hdfs_enable'])
+solr_hdfs_prefix = '#' if not solr_hdfs_enable else ''
 
 if solr_hdfs_enable:
-    solr_hdfs_prefix = '#' if not solr_hdfs_enable else ''
     solr_hdfs_directory = map_solr_hdfs['solr_hdfs_directory']
     hadoop_bin_dir = stack_select.get_hadoop_dir('bin')
     hadoop_conf_dir = conf_select.get_hadoop_conf_dir()

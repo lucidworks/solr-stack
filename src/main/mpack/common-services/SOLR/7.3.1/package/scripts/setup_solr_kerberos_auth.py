2855d1029fe0ffc91b723dfac2e82a5c920990c3
Jason Gerlowski <jason.gerlowski@lucidworks.com>
2018-05-18 11:33:16 -0400

    LWSHADOOP-757: Create 7.3.1 SOLR service
    This commit creates a SOLR mpack

---
+++ src/main/mpack/common-services/SOLR/7.3.1/package/scripts/setup_solr_kerberos_auth.py
@@ -0,0 +1,59 @@
+from resource_management.core.logger import Logger
+from resource_management.core.resources.system import Execute, File
+from resource_management.core.shell import call
+from resource_management.core.source import Template
+from resource_management.libraries.functions.format import format
+
+
+def setup_solr_kerberos_auth():
+    import params
+
+    File(format("{solr_kerberos_jaas_config}"),
+         content=Template("solr_server_jaas.conf.j2"),
+         owner=params.solr_config_user
+         )
+
+    if _has_security_json():
+        Logger.info("Solr Security Json was found, it will not be overridden")
+        return
+
+    command = format('{zk_client_prefix} -cmd put {solr_cloud_zk_directory}{security_json} ')
+    command += format('\'{solr_security_json}\'')
+    Execute(command,
+            environment={'JAVA_HOME': params.java64_home},
+            ignore_failures=True,
+            user=params.solr_config_user
+            )
+
+
+def remove_solr_kerberos_auth():
+    import params
+
+    if not _has_security_json():
+        Logger.debug(
+            format("Solr Security Json not found {solr_cloud_zk_directory}{security_json}"))
+        return
+
+    Execute(format('{zk_client_prefix} -cmd clear {solr_cloud_zk_directory}{security_json}'),
+            environment={'JAVA_HOME': params.java64_home},
+            timeout=60,
+            ignore_failures=True,
+            user=params.solr_config_user
+            )
+
+
+def _has_security_json():
+    import params
+
+    if not params.solr_cloud_mode:
+        return False
+
+    code, output = call(
+            format('{zk_client_prefix} -cmd get {solr_cloud_zk_directory}{security_json}'),
+            env={'JAVA_HOME': params.java64_home},
+            timeout=60
+    )
+
+    if "{}" == output:
+        return False
+    return "NoNodeException" not in output
---
+++ src/main/mpack/common-services/SOLR/7.3.1/package/scripts/solr_utils.py
@@ -0,0 +1,140 @@
+import os
+import re
+
+from resource_management.core.logger import Logger
+from resource_management.core.resources.system import Execute
+from resource_management.core.shell import call
+from resource_management.libraries.functions.format import format
+
+COLLECTION_PATTERN = "{solr_hdfs_directory}/[a-zA-Z0-9\._-]+"
+CORE_PATTERN = "{collection_path}\/core_node[0-9]+"
+WRITE_LOCK_PATTERN = "{0}/data/index/write.lock "
+HOSTNAME_VERIFIER_PATTERN = "{core_node_name}\":{{((?![^_]shard|core_node).)*\"node_name\":\"{solr_hostname}"
+
+
+def solr_port_validation():
+    code, output = call(
+        format('netstat -lnt | awk -v v1={solr_config_port} \'$6 == "LISTEN" && $4 ~ ":"v1\''),
+        timeout=60)
+    Logger.info(format("Solr port validation output: {output}"))
+
+    if "LISTEN" in output:
+        Logger.error(format("The port {solr_config_port} is not available"))
+        return False
+
+    return True
+
+
+def is_solr_running():
+    import params
+
+    code, output = call(
+        format('{solr_config_bin_dir}/solr status'),
+        env={'JAVA_HOME': params.java64_home},
+        timeout=60
+    )
+    Logger.info(format("Solr status output: {output}"))
+    pattern_port = re.compile(format('running on port {solr_config_port}'))
+
+    if pattern_port.search(output):
+        Logger.error(format("A Solr instance is running on port {solr_config_port}"))
+        return True
+
+    return False
+
+
+def exists_collection(collection_name):
+    import params
+
+    if not params.solr_cloud_mode:
+        if os.path.isdir(format("{solr_config_data_dir}/{collection_name}")):
+            return True
+        return False
+
+    code, output = call(format(
+        '{zk_client_prefix} -cmd get {solr_cloud_zk_directory}/collections/{collection_name}'),
+        env={'JAVA_HOME': params.java64_home},
+        timeout=60
+    )
+
+    if "NoNodeException" in output:
+        return False
+    return True
+
+
+def get_collection_paths(hadoop_output):
+    import params
+    pattern = re.compile(format(COLLECTION_PATTERN))
+
+    collection_paths = re.findall(pattern, hadoop_output)
+    return collection_paths
+
+
+def get_core_paths(hadoop_output, collection_path):
+    pattern = re.compile(format(CORE_PATTERN))
+    core_paths = re.findall(pattern, hadoop_output)
+    return core_paths
+
+
+def get_write_lock_files_solr_cloud(hadoop_prefix, collections):
+    import params
+
+    write_locks_to_delete = ''
+
+    for collection_path in collections:
+        code, output = call(format('{hadoop_prefix} -ls {collection_path}'))
+        core_paths = get_core_paths(output, collection_path)
+
+        collection_name = collection_path.replace(format('{solr_hdfs_directory}/'), '')
+        zk_code, zk_output = call(format(
+            '{zk_client_prefix} -cmd get {solr_cloud_zk_directory}/collections/{collection_name}/state.json'),
+            env={'JAVA_HOME': params.java64_home},
+            timeout=60
+        )
+        if zk_code != 0:
+            Logger.error(format('Cannot determine cores owned by [{solr_hostname}] in collection [{collection_name}] due to ZK error.'))
+            continue
+
+        for core_path in core_paths:
+            core_node_name = core_path.replace(format('{collection_path}/'), '')
+            pattern = re.compile(format(HOSTNAME_VERIFIER_PATTERN), re.MULTILINE|re.DOTALL)
+            core_on_hostname = re.search(pattern, zk_output)
+            if core_on_hostname is not None:
+                write_locks_to_delete += WRITE_LOCK_PATTERN.format(core_path)
+
+    return write_locks_to_delete
+
+
+def get_write_lock_files_solr_standalone(collections):
+    write_locks_to_delete = ''
+
+    for collection_path in collections:
+        write_locks_to_delete += WRITE_LOCK_PATTERN.format(collection_path)
+
+    return write_locks_to_delete
+
+
+def delete_write_lock_files():
+    import params
+
+    if params.security_enabled:
+        kinit_if_needed = format(
+            '{kinit_path_local} {hdfs_principal_name} -kt {hdfs_user_keytab}; ')
+    else:
+        kinit_if_needed = ''
+
+    hadoop_prefix = format('{kinit_if_needed}hadoop --config {hadoop_conf_dir} dfs')
+    code, output = call(format('{hadoop_prefix} -ls {solr_hdfs_directory}'))
+    collections = get_collection_paths(output)
+
+    if params.solr_cloud_mode:
+        write_locks_to_delete = get_write_lock_files_solr_cloud(hadoop_prefix, collections)
+    else:
+        write_locks_to_delete = get_write_lock_files_solr_standalone(collections)
+
+    if len(write_locks_to_delete) > 1:
+        Logger.info(format('For hostname: \'{solr_hostname}\' lock files \'{write_locks_to_delete}\' will be deleted.'))
+        Execute(
+            format('{hadoop_prefix} -rm -f {write_locks_to_delete}'),
+            user=params.hdfs_user
+        )
